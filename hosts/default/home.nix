6a355f6ddf6bccedc4989f37d87a61bc271375af
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2024-08-18 01:04:26 +0100

    git extracted to separated module

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -4,32 +4,15 @@
   lib,
   username,
   ...
-}: let
-  me = import ./secrets/user.nix;
-in {
+}: {
+  imports = [
+    ./modules/home/git.nix
+  ];
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "${username}";
   home.homeDirectory = "/home/${username}";
-
-  programs.git = {
-    enable = true;
-    lfs.enable = true;
-    extraConfig = {
-      init.defaultBranch = "main";
-
-      pull = {
-        rebase = true;
-      };
-
-      commit.gpgsign = true;
-      gpg.format = "ssh";
-      user.signingkey = "~/.ssh/zatevakhin-personal.github.pub";
-    };
-
-    userName = "Ivan Zatevakhin";
-    userEmail = me.personal.email;
-  };
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
