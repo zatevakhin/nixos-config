b0d07b16999cb5c6177ab8591e913510e498233d
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2024-08-18 16:48:33 +0100

    nvidia: moved to separate module

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -12,6 +12,7 @@
     ./hardware-configuration.nix
     ./modules/nixos/nix.nix
     ./modules/nixos/gaming.nix
+    ./modules/nixos/nvidia.nix
   ];
 
   sops.defaultSopsFormat = "yaml";
@@ -130,59 +131,11 @@
     pinentryPackage = pkgs.pinentry-gnome3;
   };
 
-  services.xserver.videoDrivers = ["nvidia"];
-
   environment.sessionVariables = {
   };
 
-  boot.initrd.kernelModules = ["nvidia"];
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
-
-  hardware.nvidia.prime = {
-    sync.enable = true;
-    intelBusId = "PCI:0:2:0";
-    nvidiaBusId = "PCI:1:0:0";
-  };
-
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-    # NOTE: Used to fix issues with vulkan and missing `libnvidia-gpucomp.so` in docker.
-    package = config.boot.kernelPackages.nvidiaPackages.stable.overrideAttrs {
-      src = pkgs.fetchurl {
-        url = "https://download.nvidia.com/XFree86/Linux-x86_64/535.179/NVIDIA-Linux-x86_64-535.179.run";
-        hash = "sha256-tDmkUYaZ5S7MdKqtZUEEIlBvH2WaIKIoj+MLUxizcuc=";
-      };
-    };
-
-    # NOTE: Enable if there are screen tearing issues.
-    # forceFullCompositionPipeline = true;
-  };
 
   hardware.opengl = {
     enable = true;
