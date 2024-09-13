cc8c77ff1aac4fe4ddcd136aa5225acea8c7f47d
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2024-09-13 23:59:34 +0100

    nvim: configured options, theme and gitsigns

--- hosts/default/modules/home/nixvim.nix
+++ hosts/default/modules/home/nixvim.nix
@@ -3,7 +3,45 @@
     enable = true;
     defaultEditor = true;
 
-    colorschemes.gruvbox.enable = true;
+    opts = {
+      list = true;
+      listchars = "trail:•";
+      signcolumn = "yes";
+      number = true;
+      relativenumber = true;
+      termguicolors = true;
+      shiftwidth = 4;
+      tabstop = 4;
+      softtabstop = 0;
+      expandtab = true;
+      smarttab = true;
+      cursorline = true;
+      splitbelow = true;
+      splitright = true;
+      incsearch = true;
+      ignorecase = true;
+      smartcase = true;
+      updatetime = 300;
+      swapfile = false;
+      undofile = true;
+      clipboard = {
+        register = "unnamedplus";
+        providers.xclip.enable = true;
+      };
+    };
+
     plugins.lualine.enable = true;
+
+    colorschemes.catppuccin = {
+      enable = true;
+      settings = {
+        flavour = "mocha";
+      };
+    };
+
+    plugins.gitsigns = {
+      enable = true;
+      settings.current_line_blame = true;
+    };
   };
 }
