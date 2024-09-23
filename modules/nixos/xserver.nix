c44f99293b6e724c93ac55addc6607ad84df42a1
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2024-09-23 22:46:53 +0100

    added xclip

--- modules/nixos/xserver.nix
+++ modules/nixos/xserver.nix
@@ -1,6 +1,10 @@
-{...}: {
+{pkgs, ...}: {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    xclip
+  ];
 
   # Configure keymap in X11
   services.xserver.xkb = {
