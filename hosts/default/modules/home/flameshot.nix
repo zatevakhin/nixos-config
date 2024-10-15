a52dea323bc5038fc6ea55fe7c38e87e395b8207
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2024-10-15 23:30:55 +0100

    fix: flameshot on wayland

--- hosts/default/modules/home/flameshot.nix
+++ hosts/default/modules/home/flameshot.nix
@@ -1,4 +1,9 @@
-{...}: {
+{pkgs, ...}: let
+  flameshot-gui = pkgs.writeShellScriptBin "flameshot-gui" "${pkgs.flameshot}/bin/flameshot gui";
+in {
+  # BUG: https://flameshot.org/docs/guide/wayland-help/#gnome-shortcut-does-not-trigger-flameshot
+  # FIX: https://github.com/flameshot-org/flameshot/issues/3365#issuecomment-1868580715
+
   services.flameshot = {
     enable = true;
     settings.General = {
@@ -17,7 +22,7 @@
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
       binding = "Print";
-      command = "flameshot gui";
+      command = "${flameshot-gui}/bin/flameshot-gui";
       name = "Flameshot";
     };
 
