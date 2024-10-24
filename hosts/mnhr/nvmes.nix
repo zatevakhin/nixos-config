e2c77b1630b23903fc4821cb99a275868bcb04d0
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2024-10-24 23:56:38 +0100

    chore: Added configuration for `mnhr` NVMEs JBOD.

---
+++ hosts/mnhr/nvmes.nix
@@ -0,0 +1,86 @@
+#
+# sudo mkdir -p /root/nvme.keyfile
+# sudo dd if=/dev/urandom of=/root/nvme.keyfile bs=512 count=4
+# sudo chmod 600 /root/nvme.keyfile
+#
+{
+  disko.devices = {
+    disk = {
+      nvme0 = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            data = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "cryptnvme0";
+                settings = {
+                  allowDiscards = true;
+                  keyFile = "/root/nvme.keyfile";
+                };
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/mnt/nvme0";
+                };
+              };
+            };
+          };
+        };
+      };
+      nvme1 = {
+        type = "disk";
+        device = "/dev/nvme1n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            data = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "cryptnvme1";
+                settings = {
+                  allowDiscards = true;
+                  keyFile = "/root/nvme.keyfile";
+                };
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/mnt/nvme1";
+                };
+              };
+            };
+          };
+        };
+      };
+      nvme2 = {
+        type = "disk";
+        device = "/dev/nvme2n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            data = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "cryptnvme2";
+                settings = {
+                  allowDiscards = true;
+                  keyFile = "/root/nvme.keyfile";
+                };
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/mnt/nvme2";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
