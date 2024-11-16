e47ad43f4e092b9c6ceb85e679375b78617ed26e
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2024-11-16 03:38:19 +0000

    feat: Added `AdGuard` service, PiHole is goind to be disabled.

---
+++ hosts/mnhr/containers/adguard/default.nix
@@ -0,0 +1,9 @@
+{pkgs, ...}: {
+
+  systemd.services.adguard-compose = {
+    script = "${pkgs.docker-compose}/bin/docker-compose -f ${./docker-compose.yml} up";
+
+    wantedBy = ["multi-user.target"];
+    after = ["docker.service" "docker.socket" "traefik-compose.service"];
+  };
+}
