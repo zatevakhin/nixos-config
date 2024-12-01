8e4fcee06ce92908e3c421086c7aab9e14ed2478
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2024-12-01 02:17:07 +0000

    chore: updated vaultwarden configuration

--- hosts/mnhr/containers/vaultwarden/default.nix
+++ hosts/mnhr/containers/vaultwarden/default.nix
@@ -3,11 +3,25 @@
   config,
   ...
 }: {
+  sops.secrets.vaultwarden-admin-token = {
+    sopsFile = ../../secrets/vaultwarden.yaml;
+    format = "yaml";
+    key = "admin/token";
+  };
+
+  sops.templates."vaultwarden.env".content = ''
+    ADMIN_TOKEN=${config.sops.placeholder.vaultwarden-admin-token}
+  '';
+
   systemd.services.vaultwarden-compose = {
     serviceConfig = {
+      EnvironmentFile = config.sops.templates."vaultwarden.env".path;
     };
 
     environment = {
+      SIGNUPS_ALLOWED = "false";
+      INVITATIONS_ALLOWED = "false";
+      ICON_SERVICE = "duckduckgo";
       VAULTWARDEN_DOMAIN = "vw.homeworld.lan";
       VAULTWARDEN_DATA_LOCATION = "/mnt/storage/.services/vaultwarden/data";
     };
