ae7d972781660c5db1936172fc7cc6ce0aa728a0
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2024-12-02 02:09:37 +0000

    fix: Password needed for users if users are immutable

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -62,6 +62,7 @@ in {
   sops.defaultSopsFile = ./secrets/default.yaml;
   sops.age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key" "/home/${username}/.ssh/id_ed25519"];
   sops.secrets."user/password/hashed" = {};
+  sops.secrets."user/password/hashed".neededForUsers = true;
   # </sops>
 
   # <certificates>
