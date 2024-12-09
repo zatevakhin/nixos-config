777c9cfea59d0a4d8ed18a3ed0095062d8043048
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2024-12-09 16:48:38 +0000

    flkr: using stable nvidia driver.

--- hosts/falke/modules/nixos/nvidia.nix
+++ hosts/falke/modules/nixos/nvidia.nix
@@ -36,15 +36,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-    # NOTE: Used to fix issues with vulkan and missing `libnvidia-gpucomp.so` in docker.
-    package = config.boot.kernelPackages.nvidiaPackages.stable.overrideAttrs {
-      src = pkgs.fetchurl {
-        url = "https://download.nvidia.com/XFree86/Linux-x86_64/535.179/NVIDIA-Linux-x86_64-535.179.run";
-        hash = "sha256-tDmkUYaZ5S7MdKqtZUEEIlBvH2WaIKIoj+MLUxizcuc=";
-      };
-    };
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
 
     # Screen Tearing Issues
     forceFullCompositionPipeline = true;
