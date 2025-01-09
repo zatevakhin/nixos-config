708610c9d7cda57bb28f16198db9ab70f6aae133
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-01-09 21:55:45 +0000

    chore: updated and pinned `variance`.

--- hosts/mnhr/containers/variance/default.nix
+++ hosts/mnhr/containers/variance/default.nix
@@ -5,7 +5,7 @@
   ...
 }: {
   systemd.services.variance-compose = {
-    script = "${pkgs.docker-compose}/bin/docker-compose -f ${./docker-compose.yml} up";
+    script = "${pkgs.docker-compose}/bin/docker-compose -f ${./docker-compose.yml} up --build";
 
     wantedBy = ["multi-user.target"];
     after = ["docker.service" "docker.socket" "traefik-compose.service"];
