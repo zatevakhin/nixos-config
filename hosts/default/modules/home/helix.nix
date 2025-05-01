e53f892de33defd0834d3047ceec102b8b4b4b0b
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-05-01 23:04:46 +0100

    chore: added `helix` on default to try

--- hosts/default/modules/home/helix.nix
+++ hosts/default/modules/home/helix.nix
@@ -1,13 +1,59 @@
-{...}: {
+{pkgs, ...}: {
   programs.helix = {
     enable = true;
     settings = {
+      theme = "kanagawa";
       editor = {
+        cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
         mouse = true;
         line-number = "relative";
         lsp.display-messages = true;
         file-picker.hidden = false;
       };
+    };
+    languages = {
+      language = [
+        {
+          name = "nix";
+          language-servers = ["nixd"];
+          auto-format = true;
+          formatter.command = "${pkgs.alejandra}/bin/alejandra";
+        }
+        {
+          name = "python";
+          language-servers = ["pyright" "ruff"];
+          auto-format = true;
+
+          formatter = {
+            command = "${pkgs.ruff}/bin/ruff";
+            args = ["format" "-"];
+          };
+        }
+      ];
+      language-server = {
+        nixd.command = "${pkgs.nixd}/bin/nixd";
+        ruff = {
+          command = "${pkgs.ruff}/bin/ruff";
+          args = ["server"];
+        };
+        pyright = {
+          command = "${pkgs.pyright}/bin/pyright-langserver";
+          args = ["--stdio"];
+        };
+      };
+
+      grammar = [
+        {
+          name = "python";
+        }
+        {
+          name = "nix";
+        }
+      ];
     };
   };
 }
