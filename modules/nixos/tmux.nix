6f4f2bf2c85c9bcdda42d9c246a8a6edc1436e50
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-05-04 21:36:40 +0100

    chore: `tmux` config update

--- modules/nixos/tmux.nix
+++ modules/nixos/tmux.nix
@@ -37,6 +37,14 @@ in {
       bind -n M-H previous-window
       bind -n M-L next-window
 
+      # set vi-mode
+      set-window-option -g mode-keys vi
+
+      # keybindings
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
       # Open new tmux panes in same directory
       bind '"' split-window -v -c "#{pane_current_path}"
       bind % split-window -h -c "#{pane_current_path}"
