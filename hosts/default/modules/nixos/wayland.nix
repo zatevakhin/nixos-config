e698b6ec8f20453f60377dab8bdb8bd411a073a0
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-05-24 13:45:03 +0100

    fix: cursor size

--- hosts/default/modules/nixos/wayland.nix
+++ hosts/default/modules/nixos/wayland.nix
@@ -9,7 +9,12 @@
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     QT_QPA_PLATFORM = "wayland";
+    XCURSOR_SIZE = lib.mkForce "12"; # BUG: Cursor oversized on Xwayland applications
     # NOTE: If not enough env variables from nvidia offload can be added.
+  };
+
+  environment.variables = {
+    XCURSOR_SIZE = lib.mkForce "12"; # BUG: Cursor oversized on Xwayland applications
   };
 
   boot.blacklistedKernelModules = ["i915"];
