63d862729434e56d5fbbe274d321c591500bafc2
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-05-28 22:50:49 +0100

    chore: updated `tmux` config

--- modules/nixos/tmux.nix
+++ modules/nixos/tmux.nix
@@ -48,7 +48,17 @@ in {
       # Open new tmux panes in same directory
       bind '"' split-window -v -c "#{pane_current_path}"
       bind % split-window -h -c "#{pane_current_path}"
+
+      set -g @catppuccin_flavor "mocha"                # latte, frappe, macchiato or mocha
+      set -g @catppuccin_window_status_style "slanted" # basic, rounded, slanted or none
+      set -g status-right-length 100
+      set -g status-left-length 100
+      set -g status-left ""
+      set -g status-right "#{E:@catppuccin_status_application}"
+      set -ag status-right "#{E:@catppuccin_status_session}"
+      set -ag status-right "#{E:@catppuccin_status_uptime}"
     '';
+
     plugins = with pkgs.tmuxPlugins; [
       sensible
       tmux-fzf
