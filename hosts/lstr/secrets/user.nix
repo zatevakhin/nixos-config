280d08bb5d6dfd304d0bce17311519ad1cfdf6fe
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-06-07 17:27:01 +0100

    chore(lstr): updated secrets

--- hosts/lstr/secrets/user.nix
+++ hosts/lstr/secrets/user.nix
@@ -1,7 +1,8 @@
 age-encryption.org/v1
--> ssh-ed25519 sxCCVw 24YQg9ACRq818RT5z/rU+kP4ZptpFWbO6Z7RLLhk8go
-sEsCZbpGKYxRhZskyZ94YwLfsQunC4XLDyCPJvfMXfk
--> \6R-grease k],=>c -;Mx:K6 CCI3i Y.w&qCK
-NUC6hpKr3qZ5O169Kt2zlwOPbkTbcfbw2NFqI6TwCEieORo
---- +H6ltKPIB5ad+jGDJFL4cVHYyG10Lg44TG89IN2Y6Bc
-†àZV6}ºUü;î¨ŽW`¬4Á'ÈÐ¦ñ^hƒ–p°Tjs3
+-> ssh-ed25519 sxCCVw LThLgOP/r/Q5ugTSxgqPiEcCKpqIibo6m9qlPi9dyTA
+bCfRkj/DeWJlGxqxRhxFgXGUpitbAQnBpYbcagIlUaU
+-> zPiF-grease DZ|Q8qnl )k U}6P2_ #)`][
+A7Ih98y+z2Bs5d0ELkva
+--- NdefTY+f6MEe0MaKIL4sel+T9z9xm9oFPBvvpHXdWfM
++E/B¿î²« õcÓç9£2—>ë´¥Ï·ÏBL”>¶Ð/Ã€±$81OÈB*Þ{S/Sæ¢žL7…·ao‚Ze‹CÍ±Ð3Ùå2&R‚^–^w:¢Ðt]1-K{¢uòÑïsã$:9Xh‚ƒ[
+ß°n~cõx!e3Çuökl¯}žPî^•ÿäÛi+çÜ+µ·€ü¨m/Áæ.•áyzÖ-c±åÐÐD˜ˆ'qTÎr;¡É®ª±ÊT\çršC,ÒÉSÚ/yM5È›\Æ’=3ˆ@¥uñ´W=EÙùˆÒ]îÎ£Õ…Ä©œYò£—\”>ÁÉv‰7“ç „ô®ÎËgÚ=¦¢{KÚÿ.§C­ï7O±í²B3€)°:ÃCN£„#1uŽ¢Ç—zÚ›ÑŠ—ngT§AÖUî­w4ÄmMmàæÖ«*\?/ÊzÙÙÙŒ¦ƒ™Bì p'=öÛøÕAÛGÓc¾èöÉO^ð çÏí.	Œ¾˜×ÔÓi>O–€Û°VO=VœÀbåþ0(Këš¡w^íÂ¡/°–ÊÂ{§$’“ªý…Ì6fÉÊGcÎÔ~‡<d˜8÷êWéØøžbîï-ÚÇ£ê¡à
