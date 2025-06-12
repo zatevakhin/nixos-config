587e0d5f50fc01f2114a1427f3509c9591e460c2
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-06-12 13:01:29 +0100

    fix(lstr): fixed default apps list

--- modules/home/associations.nix
+++ modules/home/associations.nix
@@ -1,26 +1,34 @@
 {...}: {
-  xdg.mimeApps.enable = true;
-  xdg.mimeApps.associations.added = {
-    "x-scheme-handler/http" = ["io.github.zen_browser.zen.desktop"];
-    "x-scheme-handler/https" = ["io.github.zen_browser.zen.desktop"];
-    "application/pdf" = ["org.gnome.Evince.desktop"];
+  xdg.mimeApps = {
+    enable = true;
 
-    "image/jpeg" = ["org.gnome.Loupe.desktop"];
-    "image/png" = ["org.gnome.Loupe.desktop"];
-    "image/gif" = ["org.gnome.Loupe.desktop"];
-    "image/webp" = ["org.gnome.Loupe.desktop"];
-    "image/tiff" = ["org.gnome.Loupe.desktop"];
-    "image/avif" = ["org.gnome.Loupe.desktop"];
-    "image/bmp" = ["org.gnome.Loupe.desktop"];
-    "image/svg+xml" = ["org.gnome.Loupe.desktop"];
-    "image/svg+xml-compressed" = ["org.gnome.Loupe.desktop"];
+    defaultApplications = {
+      "x-scheme-handler/http" = ["app.zen_browser.zen.desktop"];
+      "x-scheme-handler/https" = ["app.zen_browser.zen.desktop"];
+    };
 
-    "video/x-matroska" = ["mpv.desktop"];
-    "video/mp4" = ["mpv.desktop"];
-    "video/webm" = ["mpv.desktop"];
-    "video/quicktime" = ["mpv.desktop"];
-    "audio/ogg" = ["mpv.desktop"];
-    "audio/flac" = ["mpv.desktop"];
-    "audio/x-wav" = ["mpv.desktop"];
+    associations.added = {
+      "x-scheme-handler/http" = ["app.zen_browser.zen.desktop"];
+      "x-scheme-handler/https" = ["app.zen_browser.zen.desktop"];
+      "application/pdf" = ["org.gnome.Evince.desktop"];
+
+      "image/jpeg" = ["org.gnome.Loupe.desktop"];
+      "image/png" = ["org.gnome.Loupe.desktop"];
+      "image/gif" = ["org.gnome.Loupe.desktop"];
+      "image/webp" = ["org.gnome.Loupe.desktop"];
+      "image/tiff" = ["org.gnome.Loupe.desktop"];
+      "image/avif" = ["org.gnome.Loupe.desktop"];
+      "image/bmp" = ["org.gnome.Loupe.desktop"];
+      "image/svg+xml" = ["org.gnome.Loupe.desktop"];
+      "image/svg+xml-compressed" = ["org.gnome.Loupe.desktop"];
+
+      "video/x-matroska" = ["mpv.desktop"];
+      "video/mp4" = ["mpv.desktop"];
+      "video/webm" = ["mpv.desktop"];
+      "video/quicktime" = ["mpv.desktop"];
+      "audio/ogg" = ["mpv.desktop"];
+      "audio/flac" = ["mpv.desktop"];
+      "audio/x-wav" = ["mpv.desktop"];
+    };
   };
 }
