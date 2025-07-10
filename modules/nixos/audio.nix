7504a43a408758f30c0455ecdbe9427ebc7f5355
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-07-10 22:31:27 +0100

    chore(audio): added extra codecs for Bluetooth devices

--- modules/nixos/audio.nix
+++ modules/nixos/audio.nix
@@ -6,6 +6,32 @@
   services.pipewire = {
     enable = true;
 
+    wireplumber = {
+      enable = true;
+      extraConfig.bluetoothEnhancements = {
+        "monitor.bluez.properties" = {
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.codecs" = [
+            "sbc"
+            "sbc_xq"
+            "aac"
+            "ldac"
+            "aptx"
+            "aptx_hd"
+            "aptx_ll"
+            "aptx_ll_duplex"
+            "lc3"
+          ];
+          "bluez5.roles" = [
+            "a2dp_sink"
+            "a2dp_source"
+            "bap_sink"
+            "bap_source"
+          ];
+        };
+      };
+    };
+
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
