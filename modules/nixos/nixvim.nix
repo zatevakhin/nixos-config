5c0e72815eb3a91602aeb0c1b202b1b71bf17ee5
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-07-12 02:17:43 +0100

    chore(nixvim): `blink-cmp` instead of `nvim-cmp`

--- modules/nixos/nixvim.nix
+++ modules/nixos/nixvim.nix
@@ -584,39 +584,45 @@ in {
     };
 
     # <cmp>
-    plugins.cmp = {
+    plugins.blink-cmp = {
       enable = true;
+      setupLspCapabilities = true;
       settings = {
-        mapping = {
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
-          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        appearance = {
+          nerd_font_variant = "mono";
         };
-        sources = [
-          {name = "nvim_lsp";}
-          {name = "luasnip";}
-          {name = "path";}
-          {name = "vim-dadbod-completion";}
-          {name = "buffer";}
-        ];
-
-        snippet = {
-          expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+        keymap = {
+          preset = "enter";
+        };
+        fuzzy = {
+          implementation = "prefer_rust_with_warning";
+        };
+        completion = {
+          accept = {
+            auto_brackets = {
+              enabled = true;
+            };
+          };
+          documentation = {
+            auto_show = true;
+          };
+          ghost_text = {
+            enabled = true;
+          };
+        };
+        signature = {
+          enabled = true;
+        };
+        sources = {
+          cmdline = [];
+          lsp = {
+            fallbacks = [];
+          };
+          buffer = {};
+          path = {};
         };
       };
     };
-
-    plugins.lspkind = {
-      enable = true;
-      cmp.enable = true;
-    };
-
-    plugins.cmp-nvim-lsp.enable = true;
-    plugins.cmp-path.enable = true;
-    plugins.cmp-buffer.enable = true;
-    plugins.cmp-treesitter.enable = true;
-
-    plugins.cmp_luasnip.enable = true;
     # </cmp>
 
     # <lsp>
