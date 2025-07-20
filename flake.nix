1edd30fd36081423668bb3cc53eb9b584a730e6e
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-07-20 19:21:18 +0100

    chore: updated install Instructions

--- flake.nix
+++ flake.nix
@@ -55,9 +55,8 @@
   outputs = {
     self,
     nixpkgs,
-    nixpkgs-unstable,
     nix-darwin,
-    nix-homebrew,
+    nixpkgs-unstable,
     ...
   } @ inputs: let
     username = "ivan";
@@ -74,6 +73,28 @@
       overlays = [(import ./modules/overlays/avante-nvim.nix)];
     };
   in {
+    /*
+     * Installation Instructions:
+     *
+     * 1. NixOS Installation (using nixos-anywhere for most systems):
+     *    - Generate an ISO image if needed:
+     *      nix build .#nixosConfigurations.iso.config.system.build.isoImage
+     *    - Boot from the generated ISO.
+     *    - Prepare extra files with generated SSH keys for the target machine in 'extra-files' directory.
+     *    - Run nixos-anywhere for remote installation:
+     *      nix run github:nix-community/nixos-anywhere -- --flake .#<machine-id> --target-host root@<machine-ip> --extra-files extra-files
+     *
+     * 2. NixOS Installation (using disko for specific devices like mnhr):
+     *    - Ensure all necessary tools are present in your shell:
+     *      nix shell -- nixpkgs#{coreutils-full,dosfstools,f2fs-tools,fscrypt-experimental,gptfdisk,nixos-install-tools,util-linux,neovim}
+     *    - Install on the chosen device (e.g., for 'mnhr' on '/dev/mmcblk0'):
+     *      nix run 'github:nix-community/disko#disko-install' -- --flake .#mnhr --disk main /dev/mmcblk0
+     *
+     * 3. Nix-Darwin Installation (for macOS systems like eulr):
+     *    - Switch to the nix-darwin configuration:
+     *      nix --extra-experimental-features "nix-command flakes" run nix-darwin -- switch --flake .#eulr
+     */
+
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {
         inherit inputs;
@@ -162,12 +183,6 @@
       ];
     };
 
-    # NOTE: Install should be performed using commands below.
-    # 1. Ensure that all tools are present.
-    # >> nix shell -- nixpkgs#{coreutils-full,dosfstools,f2fs-tools,fscrypt-experimental,gptfdisk,nixos-install-tools,util-linux,neovim}
-    # 2. Install on a chosen device.
-    # >> nix run 'github:nix-community/disko#disko-install' -- --flake .#mnhr --disk main /dev/mmcblk0
-    #
     nixosConfigurations.mnhr = nixpkgs-unstable.lib.nixosSystem {
       specialArgs = {
         inherit inputs;
@@ -212,7 +227,7 @@
         inputs.nixvim.nixDarwinModules.nixvim
         inputs.sops-nix-unstable.darwinModules.sops
         inputs.home-manager-next.darwinModules.home-manager
-        nix-homebrew.darwinModules.nix-homebrew
+        inputs.nix-homebrew.darwinModules.nix-homebrew
         {
           nix-homebrew = {
             # Install Homebrew under the default prefix
@@ -231,8 +246,6 @@
       ];
     };
 
-    # NOTE: Build ISO image with specified configuration.
-    # >> nix build .#nixosConfigurations.iso.config.system.build.isoImage
     nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
       system = "${system}";
       modules = [
