482408bc5b291bde154b3289d457048258dc1f17
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-07-22 00:19:53 +0100

    chore(flkr): secrets

--- hosts/flkr/secrets/user.nix
+++ hosts/flkr/secrets/user.nix
@@ -1,3 +1,10 @@
-{
-"??"
-}
+age-encryption.org/v1
+-> ssh-ed25519 sxCCVw SqvY9ypWAc0WPdNGUdDdKASNWqa4K404uB+VLGcuyC0
+8LCnO37qRfAKaAShyZ7xCqkyyHY4Z6W7aNaVLX2heC0
+-> v(ej/-grease ( 8 J!..UP)F
+sT+NsgrWKt+dmmXCw9uIX9O5UsjNMJZ7ocLDWr4yCmmVvTIAHjDosz7+Nb8afQ8Q
+CgVptR3CvwWbN+4CCV5Vj4Bj8rZTkWY+tahm2VKKvcOZxsfWaLbZDBVSmRxlodJA
+qQQ
+--- lFcxs1YZQEP2Mg943D24L/4xncv38TtFMQpDU43Mh+s
+θª³_/?yΚΌJQαζτ©Jªsη‘Ω―ΏβªzΆen\=΅βγ	jΪΪZ‘―>xs|Ύ
+™Π|IΕ¶ÒώυsλιI0m¶όηZ¦uÿQα“γJak9Λ«χ½VÒ δήκδ›·D―Ρ{ΌλΧτ7όςLαLήJÿσ}vD­NjBε-\@Ίρε;Α§¥ω+ΕVΨ+φ#-·†µD>s`α½ά-¶vνl-L–4‹AΰªF=Ξ…OεªνΆI³µ¥ΏΜÒJ$Λ·…@gδρT]ότUδ•ςΜ‚£8
