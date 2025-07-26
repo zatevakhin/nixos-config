607ec25bcdabaede33d6793631532c681ae606ea
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-07-26 23:14:49 +0100

    fix(bluetooth): set controller mode to `bredr` because headphones trying to connect in `le` mode

--- modules/nixos/bluetooth.nix
+++ modules/nixos/bluetooth.nix
@@ -5,9 +5,8 @@
     settings = {
       General = {
         Enable = "Control,Gateway,Headset,Media,Sink,Socket,Source";
-        ControllerMode = "dual";
+        ControllerMode = "bredr";
         Experimental = true;
-        KernelExperimental = "6fbaf188-05e0-496a-9885-d6ddfdb4e03e,a6695ace-ee7f-4fb9-881a-5fac66c629af,330859bc-7506-492d-9370-9a6f0614037f,330859bc-7506-492d-9370-9a6f0614037f,330859bc-7506-492d-9370-9a6f0614037f";
       };
     };
   };
