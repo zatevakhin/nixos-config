47bc9b692793badd33583bd5d68bae4307a78459
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-08-09 20:09:29 +0100

    chore(flkr): adjusted fan curve for silence

--- hosts/flkr/modules/nixos/liquidctl.nix
+++ hosts/flkr/modules/nixos/liquidctl.nix
@@ -10,7 +10,7 @@
         "${pkgs.liquidctl}/bin/liquidctl initialize all"
         # NOTE: Seems can't control pump speed
         # "${pkgs.liquidctl}/bin/liquidctl --match '${device}' set pump speed 75"
-        "${pkgs.liquidctl}/bin/liquidctl --match '${device}' set fan speed 20 20 30 30 34 50 40 60 50 80"
+        "${pkgs.liquidctl}/bin/liquidctl --match '${device}' set fan speed 20 5  30 10  40 15  50 20  60 25  65 50  70 100"
         "${pkgs.liquidctl}/bin/liquidctl --match '${device}' set led color fixed 350017"
       ];
     };
