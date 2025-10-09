5ee00785e1a643d2eeb52572d1d32a516afcc101
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-10-09 13:38:30 +0100

    chore(lstr): updated development tools list

--- hosts/lstr/modules/nixos/development.nix
+++ hosts/lstr/modules/nixos/development.nix
@@ -1,13 +1,23 @@
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    devenv
-    direnv
-    godot
-    gcc
-    bun
-    uv
-    nodejs
-    wl-clipboard
-    jujutsu
-  ];
+{
+  pkgs,
+  pkgs-unstable,
+  ...
+}: {
+  environment.systemPackages =
+    (with pkgs; [
+      devenv
+      direnv
+      godot
+      gcc
+      bun
+      uv
+      nodejs
+      wl-clipboard
+      jujutsu
+    ])
+    ++ (with pkgs-unstable; [
+      codex
+      cursor-cli
+      claude-code
+    ]);
 }
