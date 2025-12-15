e257a5adb7c086ab53ede3d41c7208075f4306ed
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2025-12-15 14:00:10 +0000

    chore(routes): disabled

--- hosts/lstr/modules/nixos/routes.nix
+++ hosts/lstr/modules/nixos/routes.nix
@@ -1,18 +1,17 @@
 {...}: {
   # TODO: route trough AEON instead of MNHR. AEON should keep routing table for other crap in net.
-  networking.interfaces."eth2".ipv4.routes = [
-    {
-      address = "10.8.0.0";
-      prefixLength = 24;
-      via = "192.168.1.10";
-    }
-  ];
-
-  networking.interfaces."wlp195s0".ipv4.routes = [
-    {
-      address = "10.8.0.0";
-      prefixLength = 24;
-      via = "192.168.1.10";
-    }
-  ];
+  # networking.interfaces."eth2".ipv4.routes = [
+  #   {
+  #     address = "10.8.0.0";
+  #     prefixLength = 24;
+  #     via = "192.168.1.10";
+  #   }
+  # ];
+  # networking.interfaces."wlp195s0".ipv4.routes = [
+  #   {
+  #     address = "10.8.0.0";
+  #     prefixLength = 24;
+  #     via = "192.168.1.10";
+  #   }
+  # ];
 }
