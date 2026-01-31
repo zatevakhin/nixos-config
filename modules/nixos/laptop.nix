ae35c73f58a02ba7725ee6efc73a2ab2f99413b5
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2026-01-31 16:19:01 +0000

    fix: use stable tlp

--- modules/nixos/laptop.nix
+++ modules/nixos/laptop.nix
@@ -13,7 +13,6 @@
 
   services.tlp = {
     enable = true;
-    package = pkgs-unstable.tlp;
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
