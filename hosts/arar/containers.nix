c12b7fd5292d3ef7325bb87d37c00ae7c93e1f27
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2026-06-20 01:28:52 +0100

    fix(arar): containers

--- hosts/arar/containers.nix
+++ hosts/arar/containers.nix
@@ -1,6 +1,8 @@
-{...}: {
+{self, ...}: {
   flake.nixosModules.arar-containers = {...}: {
     imports = [
+      self.nixosModules.container-calibre-web
+      self.nixosModules.container-paperless-ngx
     ];
   };
 }
