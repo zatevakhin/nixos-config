7b536f4dbac2cbca6c24e86c81f77f58f58954bd
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2026-08-20 19:27:14 +0100

    chore: file associations

--- modules/home/gnome/associations.nix
+++ modules/home/gnome/associations.nix
@@ -6,12 +6,22 @@
       defaultApplications = {
         "x-scheme-handler/http" = ["app.zen_browser.zen.desktop"];
         "x-scheme-handler/https" = ["app.zen_browser.zen.desktop"];
+        "text/plain" = ["nvim.desktop"];
+        "text/csv" = ["nvim.desktop"];
       };
 
       associations.added = {
         "x-scheme-handler/http" = ["app.zen_browser.zen.desktop"];
         "x-scheme-handler/https" = ["app.zen_browser.zen.desktop"];
         "application/pdf" = ["org.gnome.Evince.desktop"];
+        "application/json" = ["nvim.desktop"];
+        "text/markdown" = ["nvim.desktop"];
+        "text/plain" = ["nvim.desktop"];
+        "text/html" = ["nvim.desktop"];
+        "text/csv" = ["nvim.desktop"];
+
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = ["org.onlyoffice.desktopeditors.desktop"];
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = ["org.onlyoffice.desktopeditors.desktop"];
 
         "image/jpeg" = ["org.gnome.Loupe.desktop"];
         "image/png" = ["org.gnome.Loupe.desktop"];
