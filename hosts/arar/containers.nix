24417c3216500233a581554c0dbe193fd5598a6c
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2026-08-26 23:17:01 +0100

    feat(arar): deploy copy of vaultwarden

--- hosts/arar/containers.nix
+++ hosts/arar/containers.nix
@@ -1,8 +1,17 @@
 {self, ...}: {
-  flake.nixosModules.arar-containers = {...}: {
+  flake.nixosModules.arar-containers = {
+    config,
+    lib,
+    ...
+  }: {
     imports = [
       self.nixosModules.container-calibre-web
       self.nixosModules.container-paperless-ngx
+      self.nixosModules.container-vaultwarden
     ];
+
+    config.systemd.services.vaultwarden-compose.environment = {
+      VAULTWARDEN_DATA_LOCATION = lib.mkForce "/mnt/storage/.services/vaultwarden/data";
+    };
   };
 }
---
+++ modules/containers/vaultwarden/default.nix
@@ -0,0 +1,56 @@
+{...}: {
+  flake.nixosModules.container-vaultwarden = {
+    pkgs,
+    config,
+    hostname,
+    ...
+  }: let
+    domain = "vw-${hostname}.homeworld.lan";
+  in {
+    services.adguardhome.settings.filtering.rewrites = [
+      {
+        domain = domain;
+        answer = "${hostname}.lan";
+        enabled = true;
+      }
+    ];
+    sops.secrets.vaultwarden-admin-token = {
+      sopsFile = ../../../secrets/${hostname}/vaultwarden.yaml;
+      format = "yaml";
+      key = "admin/token";
+    };
+
+    sops.templates."vaultwarden.env".content = ''
+      ADMIN_TOKEN=${config.sops.placeholder.vaultwarden-admin-token}
+    '';
+
+    systemd.services.vaultwarden-compose = {
+      serviceConfig = {
+        EnvironmentFile = config.sops.templates."vaultwarden.env".path;
+      };
+
+      environment = {
+        SIGNUPS_ALLOWED = "false";
+        ENABLE_WEBSOCKET = "true";
+        INVITATIONS_ALLOWED = "false";
+        ICON_SERVICE = "duckduckgo";
+        INTERNAL_DOMAIN_NAME = domain;
+        VAULTWARDEN_DATA_LOCATION = "/storage/.services/vaultwarden/data";
+      };
+
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.docker-compose}/bin/docker-compose --file ${./docker-compose.yml} up";
+        ExecStop = "${pkgs.docker-compose}/bin/docker-compose --file ${./docker-compose.yml} stop";
+        StandardOutput = "syslog";
+        Restart = "on-failure";
+        RestartSec = 5;
+        StartLimitIntervalSec = 60;
+        StartLimitBurst = 3;
+      };
+
+      wantedBy = ["multi-user.target"];
+      after = ["docker.service" "docker.socket" "traefik.service"];
+    };
+  };
+}
